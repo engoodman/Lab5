b5f677613ef374307dd355b658749c18ff9a4f22
mollywigandt <mollywigandt@wifi-roaming-128-4-142-250.host.udel.edu>
2016-11-11 14:30:35 -0500

    Part 7

--- RioClient/src/main/java/poker/app/view/PokerTableController.java
+++ RioClient/src/main/java/poker/app/view/PokerTableController.java
@@ -1,6 +1,8 @@
 package poker.app.view;
 
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.animation.FadeTransition;
@@ -18,6 +20,7 @@ import javafx.scene.layout.HBox;
 import javafx.util.Duration;
 import poker.app.MainApp;
 import pokerBase.Action;
+import pokerBase.Player;
 import pokerBase.Table;
 import pokerEnums.eAction;
 import pokerEnums.ePlayerPosition;
@@ -138,21 +141,79 @@ public class PokerTableController implements Initializable {
 
 	public void Handle_TableState(Table HubPokerTable) {
 
-		//TODO: If this message is called, that means there
-		//		was a change to the state of the Table (player
-		//		probably ran 'sit' or 'leave'
-		//		The Table was updated, you just have to refresh the
-		//		UI controls to show the current state of the 
-		//		Table object
-		
-		//TODO: run the 'getHashPlayers' method, iterate 
-		//		for all players and update the player label
-		//		and state of the sit/leave button.
+		lblPos1Name.setText("");
+		lblPos2Name.setText("");
+		lblPos3Name.setText("");
+		lblPos4Name.setText("");
 
-		//		Example: Joe sits at Position 1
-		//		Joe should see the 'Sit' button in position 1 in the
-		//		'pressed in' state, and with 
+		//scanInputControls(OuterBorderPane, "SitLeave",true);
+		
+		btnPos1SitLeave.setVisible(true);
+		btnPos2SitLeave.setVisible(true);
+		btnPos3SitLeave.setVisible(true);
+		btnPos4SitLeave.setVisible(true);
+
+		btnPos1SitLeave.setText(btnPos1SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos2SitLeave.setText(btnPos2SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos3SitLeave.setText(btnPos3SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos4SitLeave.setText(btnPos4SitLeave.isSelected() ? "Leave" : "Sit");
+
+		btnStartGame.setDisable(HubPokerTable.getHashPlayers().size() > 0 ? false : true);
+
+		FadeButton(btnStartGame);
+		Iterator iterator = HubPokerTable.getHashPlayers().entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry pair = (Map.Entry) iterator.next();
+			Player p = (Player) pair.getValue();
+			switch (p.getiPlayerPosition()) {
+			case 1:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(true);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos1SitLeave.setVisible(false);
+				}
+				lblPos1Name.setText(p.getPlayerName().toString());
+				break;
+			case 2:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(true);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos2SitLeave.setVisible(false);
+				}
+				lblPos2Name.setText(p.getPlayerName().toString());
+				break;
+			case 3:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(true);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos3SitLeave.setVisible(false);
+				}
+				lblPos3Name.setText(p.getPlayerName().toString());
+				break;
+			case 4:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(true);
+				} else {
+					btnPos4SitLeave.setVisible(false);
+				}
+				lblPos4Name.setText(p.getPlayerName().toString());
+				break;
+			}
+		}
 	}
+	
 
 	@FXML
 	void btnStart_Click(ActionEvent event) {
